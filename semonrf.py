e131d4966a053d647069bcdee39facf41c345ffd
Royalforce71 <65408605+Royalforce71@users.noreply.github.com>
2020-05-15 21:26:15 +0600

    Update and rename zeroboy.py to semonrf.py

--- semonrf.py
+++ semonrf.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•SEMON-MEHRAN------•◈•
+|__\~~) •||•FREE-TE-MARA-KHAO--------------•◈•
+|__(-----\  •◈•------ROYAL-FORCE 71--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mSemon
+\033[1;96mInstagram \033[1;93m: \033[1;call_me_semon
+\033[1;96mFacebook  \033[1;93m: \033[1; SayHi2Semu
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Royalforce71/semonrf
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mSemon
+\033[1;96mInstagram \033[1;93m: \033[1;92mfuckme
+\033[1;96mFacebook  \033[1;93m: \033[1;92m SayHi2Semu
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Royalforce71/semonrf
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "royalforce71"
+CorrectPassword = "semonrf"
 
 loop = 'true'
 while (loop == 'true'):
